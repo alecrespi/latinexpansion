9eb0ebc67ccdd791aedb7894cabfe58dd49cc167
Alessandro Crespi <alessandro.crespi.2002@gmail.com>
2024-07-12 06:30:35 +0200

    final version except for some minor corrections

--- src/clib/latinexpansionmodule.c
+++ src/clib/latinexpansionmodule.c
@@ -9,49 +9,81 @@
 #include <numpy/ndarrayobject.h>
 #include <numpy/ndarraytypes.h>
 
-#define U ((double)rand() / (double)RAND_MAX)   // draws a double number between 0 and 1
 #define I(q) (((double)q)/(N+M))                // given q interval index, get interval lower bound
 #define Q(x) (floor(((double)x)*(N+M)))         // given x sample, get interval index
-#define R(q) ((U%I(1)) + I((q)))                // random number between I(q) and I(q+1)
+#define SWAP(a, b) do { typeof(a) tmp = a; a = b; b = tmp; } while (0)  // var swapping
+#define R(q) ((double)I(q) + ((double)rand() * I(1) / RAND_MAX))
 
 typedef struct vlist{
     int len;
     int* vacancies;
 } vacancies_list;
 
-int **vacancy_space_reduction(vacancies_list*, int, int);
+double euclid(double*, double*, int);
+double min_distance(double**, double**, int, int, int);
+void print_double_matrix(double**, int, int);
+void print_int_matrix(int**, int, int);
+int **shuffle_subset_vacancies(vacancies_list*, int, int);
 vacancies_list* build_vacancies_matrix(double**, int, int, int);
 double** load_sample_set(PyObject*, int*, int*);
 PyObject* build_result_matrix(double**, int, int);
 void free_matrix(void**, int);
 
-srand((unsigned)time(NULL)); 
-
 //--------- CORE MODULE --------
 //------------------------------
-static PyObject* method_eLHS(PyObject* self, PyObject* args) {
+static PyObject* method_eLHS(PyObject* self, PyObject* args, PyObject* kwargs) {
     PyObject *matrix_object;
-    // ss = sample set; exp = expansion set
-    double **ss, **exp; 
-    int N, M, P;
+    vacancies_list *vs;
+    int **v_prime; 
+    double **ss, **exp; /* sample set, expansion set */
+    double max_distance, dist;
+    int i, j, t, N, M, P, throws = 10, criteria = 1;
+    // criteria = 0 : RANDOM ; criteria = 1 : maximize minimum distance
+    static char *kwlist[] = {"sample_set", "M", "throws", "criteria", NULL};
 
-    if (!PyArg_ParseTuple(args, "Oi", &matrix_object, &M))  
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "Oi|ii", kwlist, &matrix_object, &M, &throws, &criteria))
         return NULL;
+
+    if(M == 0)
+        return build_result_matrix(exp, 0, 0);
     
     ss = load_sample_set(matrix_object, &N, &P);
     if(!ss) return NULL;
 
+    if(criteria == 0)
+        throws = 1;
+
     // METHOD'S BODY
-    vacancies_list *vs = build_vacancies_matrix(ss, N, P, M);
+    exp = (double **)malloc(M * sizeof(double*));
+    for(i = 0; i < M; i++)
+        exp[i] = (double *)malloc(P * sizeof(double));
 
+    vs = build_vacancies_matrix(ss, N, P, M);
 
-    
-    // yielding result
-    // PyObject* result = build_result_matrix(ss, N, P);
-    PyObject* result = build_result_matrix(ss, N, P);
+    // maximin distance
+    max_distance = 0.0;
+    for(t = 0; t < throws; t++){
+        // shuffle and subset vacancies set
+        v_prime = shuffle_subset_vacancies(vs, M, P);
+        
+        // sowing new points
+        for(i = 0; i < M; i++)
+            for(j = 0; j < P; j++)
+                exp[i][j] = R(v_prime[j][i]);
+        
+        if(throws != 1){
+            dist = min_distance(ss, exp, N, M, P);
+            if(dist > max_distance)
+                max_distance = dist;
+        }
+    }
+
+    // handling result
+    PyObject* result = build_result_matrix(exp, M, P);
+    free(vs);
+    free_matrix((void**) v_prime, P);
     free_matrix((void**) ss, N);
-    // return result;
-    Py_RETURN_NONE;
+    return result;
 }
 
 static PyObject* method_grade(PyObject* self, PyObject* args, PyObject* kwargs) {
@@ -90,31 +122,115 @@ static PyObject* method_grade(PyObject* self, PyObject* args, PyObject* kwargs) 
 //-------------------------------
 //ˆˆˆˆˆˆˆˆˆ CORE MODULE ˆˆˆˆˆˆˆˆˆ
 
-int **vacancy_space_reduction(vacancies_list* vs, int M, int P){
-    return NULL;
+double euclid(double* p1, double* p2, int P) {
+    double sum = 0.0;
+    for (int i = 0; i < P; i++) 
+        sum += pow(p1[i] - p2[i], 2);
+    return sqrt(sum);
+}
+
+double min_distance(double **ss, double **exp, int N, int M, int P){
+    int i, k;
+    double min = __DBL_MAX__, dist = 0.0;
+    for(k = 0; k < M; k++){
+        for(i = 0; i < N; i++){
+            dist = euclid(exp[k], ss[i], P);
+            if( dist < min ) min = dist;
+        }
+        for(i = k + 1; i < M; i++){
+            dist = euclid(exp[k], exp[i], P);
+            if( dist < min ) min = dist;
+        }
+    }
+    return min;
+}
+
+int** transpose_voids(int** original, int P, int M) {
+    int **transposed, i, j;
+    transposed = (int **)malloc(M * sizeof(int*));
+    for (i = 0; i < M; i++) 
+        transposed[i] = (int *)malloc(P * sizeof(int));
+
+    for (j = 0; j < P; j++) 
+        for (i = 0; i < M; i++) 
+            transposed[i][j] = original[j][i];
+        
+    return transposed;
+}
+
+void print_double_matrix(double **m, int rows, int cols){
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < cols; j++)
+            printf("%.12lf |", m[i][j]);
+        printf("\n");
+    }
+}
+void print_int_matrix(int **m, int rows, int cols){
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < cols; j++)
+            printf("%d |", m[i][j]);
+        printf("\n");
+    }
+}
+
+// subsets vs rows to M elements and shuffle them
+int **shuffle_subset_vacancies(vacancies_list* vs, int M, int P){
+    if(!vs || M == 0) return NULL;
+    int **v_prime = (int **)malloc(P * sizeof(int*));
+
+    /* for each dimension, shuffle and reshape the vacancy list to a regular size M */
+    for(int j = 0; j < P; j++){
+        if(!vs[j].vacancies || !vs[j].len)
+            return NULL;
+
+        int *vptr = (int*)malloc(vs[j].len * sizeof(int));
+        /* copy elements to preserve data integrity for further steps*/
+        for(int i = 0; i < vs[j].len; i++)
+            vptr[i] = vs[j].vacancies[i];
+
+        /* shuffling all items */
+        for (int i = vs[j].len; i > 0; i--){ 
+            // generate a random index that ranges from [0, M-1]
+            int r = rand() % i;     
+            SWAP(vptr[i-1], vptr[r]);
+        }
+        /* truncate the list to M */
+        vptr = (int *)realloc(vptr, M*sizeof(int)); 
+        v_prime[j] = vptr;
+    }
+    return v_prime;
 }
 
 vacancies_list* build_vacancies_matrix(double** ss, int N, int P, int M){
     vacancies_list *vs;
     int i, j, q;
 
+    // setting up space for result
     vs = (vacancies_list*)malloc(P*sizeof(vacancies_list));
 
     for(j = 0; j < P; j++){
         int vindex = 0;
         int *vptr = (int*) malloc((N+M)*sizeof(int));
-        bool *vmask = (bool*) malloc((N+M)*sizeof(bool));
-        memset(vmask, 0, (N + M) * sizeof(bool));
+        /* masks the void (if index is FALSE) indexes of vptr  */
+        bool *vmask = (bool*) malloc((N+M)*sizeof(bool));   
+        /* set mask to false */
+        memset(vmask, 0, (N + M) * sizeof(bool));           
         
+        // map down the busy intervals (busy is TRUE)
         for(i = 0; i < N; i++)
             vmask[(int) Q(ss[i][j])] = true;
         
+        // mask the interval indexes to gather only voids
         for(q = 0; q < N+M; q++){
-            if(!vmask[q])   // if q IS void
+            // if q is a void, then add it
+            if(!vmask[q])   
                 vptr[vindex++] = q;
         }
 
+        // realloc voids sequence to fit void numbers
         vptr = (int*) realloc(vptr, vindex * sizeof(int));
+        if(!vptr)   return NULL;
+        // build up vacancies_list entry
         vs[j].len = vindex;
         vs[j].vacancies = vptr;
     }
@@ -173,14 +289,14 @@ double** load_sample_set(PyObject* matrix_object, int* N, int* P){
     return c_matrix;
 }
 
-PyObject* build_result_matrix(double **matrix, int M, int P){
-    PyObject* result = PyList_New(M);
+PyObject* build_result_matrix(double **matrix, int nrows, int ncols){
+    PyObject* result = PyList_New(nrows);
     int i,j;
-    for (i = 0; i < M; i++) {
-        PyObject* row = PyList_New(P);
-        for (j = 0; j < P; j++) 
+
+    for (i = 0; i < nrows; i++) {
+        PyObject* row = PyList_New(ncols);
+        for (j = 0; j < ncols; j++) 
             PyList_SetItem(row, j, PyFloat_FromDouble(matrix[i][j]));
-        
         PyList_SetItem(result, i, row);
     }
     return result;
@@ -195,7 +311,7 @@ void free_matrix(void **m, int N){
 
 // module functions to export
 static PyMethodDef latinexpansionMethods[] = {
-    {"eLHS", method_eLHS, METH_VARARGS, "Expansion algorithm for Latin Hypercube Sampling."},
+    {"eLHS", (PyCFunction)method_eLHS, METH_VARARGS | METH_KEYWORDS, "Expansion algorithm for Latin Hypercube Sampling."},
     {"grade", (PyCFunction)method_grade, METH_VARARGS | METH_KEYWORDS, "Non-collapsing property metric for sample set."},
     {NULL, NULL, 0, NULL}
 };
@@ -211,6 +327,7 @@ static struct PyModuleDef latinexpansionModule = {
 
 // module initialization routine (ONLY declared, NOT invoked)
 PyMODINIT_FUNC PyInit_latinexpansion(void) {
+    srand((unsigned int)time(NULL));
     import_array();  /* Initialize the NumPy C API */
     return PyModule_Create(&latinexpansionModule);
 }
--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,18 @@
 from scipy.stats.qmc import LatinHypercube as LHSampler
-from matplotlib import pyplot as plt
+from matplotlib import cm, pyplot as plt
 import numpy as np
 from math import floor
 import random as r
 from decimal import Decimal
+import json
+from typing import Callable, Generator
+import ipywidgets as ws
+from IPython.display import display
+from time import time
+from IPython.display import clear_output
 
 # 2D-only LHS plotter
-def plotLHS(lhs: np.ndarray, grid: bool = False, highlight: bool = False, save: bool = False, filepath: str = None):
+def plotLHS_old(lhs: np.ndarray, grid: bool = False, highlight: bool = False, save: bool = False, filepath: str = None):
     N, P = lhs.shape
     if P != 2:
         return
@@ -35,9 +41,6 @@ def plotLHS(lhs: np.ndarray, grid: bool = False, highlight: bool = False, save: 
     else:
         plt.show()
 
-
-
-
 # other utilities 
 def concat(a: np.ndarray, b:np.ndarray):
     return np.concatenate((a, b), axis=0)
@@ -65,3 +68,202 @@ def high_precision_difference(a: float, b: float, rounding_error = 30):
         float(Decimal(str(a)) - Decimal(str(b))), 
         rounding_error
     )
+
+
+###################################
+
+# minimum distance metric
+def mindist(points):
+    points = np.array(points)
+    def dist_v_m(point:np.ndarray, pointset:np.ndarray):
+        return np.min(np.linalg.norm((pointset - point).T, axis=0))
+    def v_m(i:int):
+        return points[i], np.vstack((points[:i], points[(i+1):]))
+    return np.min([
+        dist_v_m(*v_m(i)) for i in range(len(points))
+    ])
+
+#utility functions meant to save experiments
+class Experiment:
+    def __init__(self, id:str, data, description:str):
+        self.id = id
+        self.datalist = data
+        self.description = description
+    
+    def toJSON(self):
+        return json.dumps(
+            self,
+            default=lambda o: o.__dict__, 
+            sort_keys=True,
+            indent=0)
+    
+    def instanceToJSON(obj):
+        obj = Experiment(obj["id"], obj["datalist"], obj["description"])
+        return obj.toJSON()
+
+def expsave(id:str, data, mode:str, description: str = ""):
+    if(mode == "w"):
+        with open("./data/" + id + ".json", "w+") as f:
+            experiment = Experiment(id, [data], description)
+            f.write(experiment.toJSON())
+    elif(mode == "a"):
+        prevexp = expget(id)
+        prevexp["datalist"].append(data)
+        if description != "" and description is not None:
+            prevexp["description"] = description
+        with open("./data/" + id + ".json", "w+") as f:
+            f.write(Experiment.instanceToJSON(prevexp))
+
+def expget(id:str):
+    f = open("./data/" + id + ".json", "r")
+    return json.loads(f.read())
+
+# monte-carlo simulation
+def MCSim(f:Callable, xs: np.ndarray, V: float):
+    return V * np.mean(np.apply_along_axis(f, 1, xs))
+
+# numerical integration class for P functions
+class midpointIntegration:
+    def __init__(self, d:int):
+        if d <= 0:
+            raise ValueError("Dimension must be positive")
+        self.d = d
+    
+    def __gen_indexes(self, Ns, borders = False) -> Generator[list, None, None]:
+        index = [0] * self.d
+        if not borders:
+            Ns = (np.array(Ns) - 1).tolist()
+        yield index
+        for _ in range(np.prod(Ns)):
+            for p in reversed(range(self.d)):
+                if index[p] + 1 == Ns[p]:
+                    if p == 0:
+                        return
+                    index[p] = 0
+                else:
+                    index[p] += 1
+                    break
+            yield index
+
+    def __relindex(self, abs_index: int, Ns: list[int]):
+        rel = np.zeros((self.d), dtype=int)
+        r = abs_index
+        for p in range(0, self.d):
+            rel[p] = floor(r/Ns[p])
+            r = r % Ns[p]
+        rel[-1] = r
+        return rel
+
+    def __absindex(self, rel_index:list[int], Ns:list[int]):
+        abs = 0
+        for p in range(0, self.d - 1):
+            abs += Ns[p] * rel_index[p]
+        abs += rel_index[-1]
+        return abs
+
+    def __shift_on_axis(self, index, axis: int, Ns: list[int], increment: int = 1):
+        if type(index) is int:
+            index = self.__relindex(index, Ns)
+        rtr = [*index]
+        rtr[axis] += increment
+        if type(index) is int:
+            return self.__absindex(rtr)
+        else:
+            return rtr
+
+    def midpoint(self, xs, index: list[int], Ns: list[int]):
+        m = np.zeros((self.d))
+        for p in range(self.d):
+            shifted_index = self.__shift_on_axis(index, p, Ns)
+            m[p] = (xs[*index, p] + xs[*shifted_index, p])/2
+        return m
+
+    # lbs = lower_boundaries
+    # ubs = upper_boundaries
+    def integrate(self, f:Callable, Ns:list[int], lbs: list[int], ubs: list[int], xs = None):
+        if len(Ns) != self.d:
+            raise ValueError("Ns must be a list of integers ", self.d, " long")
+        
+        if xs is None:
+            xs = np.zeros((*Ns, self.d))
+        for i in self.__gen_indexes(Ns, borders = True):
+            xs[*i] = [((i[k]/Ns[k])*(ubs[k] - lbs[k]))+lbs[k] for k in range(self.d)]
+            # xs[*i] = np.linspace(lbs[], ubs[k],)
+         
+        I = 0
+        for i in self.__gen_indexes(Ns, borders = False):
+            I += f(self.midpoint(xs, i, Ns))
+        I *= 1/np.prod(Ns)
+        return I
+
+class MC:
+    def __init__(self, P):
+        self.P = P
+
+    def random(self, N:int) -> np.ndarray:
+        return np.random.rand(N, self.P)
+    
+def plot3d(
+    X:np.ndarray, Y:np.ndarray, Z:np.ndarray, 
+    elev=100, azim=30, color_bar=False, 
+    save=False, fp=None):
+    
+    def save_plot(_):
+        exp_id = fpInput.value if fp is None else fp
+        if exp_id is not None and exp_id != "":
+            fig = livePlot3d(
+                elev=elevSlider.value, 
+                azim=azimSlider.value
+            )
+            fig.savefig("./data/plots/" + exp_id + ".png", dpi=500)
+
+    saveButton = ws.Button(description="SAVE")
+    saveButton.on_click(save_plot)
+
+    fpInput = ws.Text(
+        value='',
+        placeholder='ID ONLY, do not include file extension and path',
+        description='Experiment ID: ',
+        disabled=False
+    )
+
+    elevSlider = ws.IntSlider(
+        value=elev,
+        min=-90,
+        max=90,
+        step=1,
+        description='Elevation:'
+    )
+    azimSlider = ws.IntSlider(
+        value=azim,
+        min=-90,
+        max=90,
+        step=1,
+        description='Azimuth:'
+    )
+    
+    def livePlot3d(elev, azim):
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')   
+
+        ax.view_init(elev=elev, azim=azim)
+        if color_bar:
+            fig.colorbar(surf, shrink=0.5, aspect=5)
+        # Plot the surface.
+        surf = ax.plot_surface(
+            X, Y, Z, 
+            cmap=cm.coolwarm,
+            linewidth=0, 
+            antialiased=False,
+            # rcount=len(X), ccount=len(Y),
+            # rstride = 100, cstride = 100
+        )
+        return fig
+
+    if save:
+        display(saveButton)
+        if fp is None:
+            display(fpInput)
+    
+    ws.interact(livePlot3d, elev=elevSlider, azim=azimSlider)
+
